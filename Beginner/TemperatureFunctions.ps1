bd2ed9d0f601a0517c34d2da215187a55f08d414
blakebourgeois <blake@quicksand.tech>
2020-11-25 15:10:13 -0600

    temperature challenge

---
+++ Beginner/TemperatureFunctions.ps1
@@ -0,0 +1,25 @@
+﻿## Iron Scripter Challenge https://ironscripter.us/a-beginner-powershell-function-challenge/
+
+function ConvertTo-Celcius($tempinF){
+    $type = $tempinF.getType().Name
+    if(($type -eq "Int32") -or ($type -eq "Double")){
+        $tempinC = ($tempinF - 32)/1.8
+        return $tempinC
+        }
+    else{
+        Write-Host "Invalid number provided!" -ForegroundColor Red
+        return $null
+        }
+    }
+
+function ConvertTo-Fahrenheit($tempinC){
+    $type = $tempinC.getType().Name
+    if(($type -eq "Int32") -or ($type -eq "Double")){
+        $tempinF = ($tempinC * 1.8) + 32
+        return $tempinF
+        }
+    else{
+        Write-Host "Invalid number provided!" -ForegroundColor Red
+        return $null
+        }
+    }
