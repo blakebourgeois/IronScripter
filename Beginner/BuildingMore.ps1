bb6b134f631329de1e11c1293e82a0742b32205f
blakebourgeois <blake@quicksand.tech>
2020-11-25 14:22:01 -0600

    more base challenges

---
+++ Beginner/BuildingMore.ps1
@@ -0,0 +1,46 @@
+﻿## Iron Scripter Challenge https://ironscripter.us/building-more-powershell/
+
+## TO DO ##
+# progress bar for folders
+# display hidden files separately
+# exclude file extensions
+# custom type extension for size/average in kb
+# module: maml based external help file
+# custom formatting view that shows computername, path, total files, total file size in KB, date
+
+function Get-PathDetails{
+    
+    # get path from user
+    $path = Read-Host "Enter Path"
+
+    # validate the path exists
+    if(get-item $path){
+        # get recurvively get files from directory
+        $files = gci $path -Recurse -Force
+
+        # count the files
+        $filecount = ($files | Measure-Object).count
+        $size = 0
+
+        # add up all the file lengths
+        foreach($file in $files){
+            $size += $file.length
+            }
+
+        # make the size and average pretty
+        ## modified from MB to KB as per challenge
+        $size = $size/1KB
+        $size = [math]::Round($size,2)
+        $avg = [math]::Round($size/$filecount,2)
+        $date = get-date -f "yyyy-MM-dd"
+
+        # output
+        write-host "There are $filecount files in the path, totaling $size MB with an average of $avg MB per file. This command was run on" $env:COMPUTERNAME "on $date"
+    }
+    else{
+        # error handling for non-existent path
+        Write-Host "That path doesn't exist." -foregroundcolor Red
+        }
+}
+
+Set-Alias -Name IronScripter1 -Vaule Get-PathDetails
---
+++ Beginner/FeatureChallenge.ps1
@@ -0,0 +1,40 @@
+﻿## Iron Scripter Challenge https://ironscripter.us/a-windows-feature-powershell-challenge/
+
+##TO DO##
+# multiple features through pipeline
+# incorporate whatif
+# incorporate logging
+# improve error handling
+# configure to run on multiple remote computers
+# autocomplete feature names
+
+function Manage-MyFeatures{
+    Param(
+        [Parameter(Mandatory=$true)]
+        [String]
+        $myFeature,
+
+        [Parameter(Mandatory=$true)]
+        [boolean]
+        $enable
+    )
+    
+    $feature = Get-WindowsOptionalFeature -Online -FeatureName $myFeature
+    
+    if($enable -eq $true){
+        if($feature.State -eq "Enabled"){
+            write-host "This feature is already enabled."  
+            }
+        else{
+            Enable-WindowsOptionalFeature -Online -FeatureName $myFeature
+            }
+        }
+    if($enable -eq $false){
+        if($feature.State -eq "Disabled"){
+            write-host "This feature is already disabled."
+            }
+        else{
+            Disable-WindowsOptionalFeature -Online -FeatureName $myFeature
+            }
+        }
+    }
