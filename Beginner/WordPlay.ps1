4acb7f26a24a4acda8b9053cea334eb7660eaba9
blakebourgeois <blake@quicksand.tech>
2020-11-28 08:35:18 -0600

    wordplay challenge

---
+++ Beginner/WordPlay.ps1
@@ -0,0 +1,68 @@
+﻿## Iron Scripter Challenge https://ironscripter.us/a-powershell-word-play-challenge/
+
+#$myString = "powershell practice"
+$myString = ""
+
+#beginner challenge
+function ConvertTo-TotalAndString{
+    param(
+        [parameter(
+        Mandatory = $true,
+        ValueFromPipeline = $true)]
+        $myString
+    )
+
+    $total = 0
+    $myNewString = ""
+    $count = 0
+    while($count -lt $myString.Length){
+        $charInt = [char]$myString[$count] -as [int]
+        $total += $charInt
+        $myNewString += [string]$charInt + " "
+        $count++
+        }
+
+    $total
+    $myNewString
+    }
+
+#advanced challenge
+function ConvertTo-DoubledChar{
+    param(
+        [parameter(
+        Mandatory = $true,
+        ValueFromPipeline = $true)]
+        $myString
+    )
+
+    $myNewString = ""
+    $count = 0
+    while($count -lt $myString.Length){
+        $charInt = [char]$myString[$count] -as [int]
+        $charInt = $charInt*2
+        $myNewString += [char]$charInt
+        $count++
+        }
+
+    return $myNewString
+    }
+
+function ConvertFrom-DoubledChar{
+    param(
+        [parameter(
+        Mandatory = $true,
+        ValueFromPipeline = $true)]
+        $myString
+    )
+
+    $myNewString = ""
+    $count = 0
+    while($count -lt $myString.Length){
+        $charInt = [char]$myString[$count] -as [int]
+        $charInt = $charInt/2
+        $myNewString += [char]$charInt
+        $count++
+        }
+
+    return $myNewString
+    }
